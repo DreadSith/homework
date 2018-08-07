c70795e4b11347c6ad5000de3040fccfd1c40db5
DreadSith <raveningly@gmail.com>
2018-08-07 03:27:43 +0300

    Second commit.
    Set up MainController and SecurityConfiguration. I can now both login and logout, and incorrect login will display an appropriate message. Made h2 db console play nice with Spring Security. TODO Figure out how to use h2 db, set up blog entries table, figure out how to display everything and perform several crud operations. OPTIONAL Think about the visual presentation more, add password encoding.

---
+++ src/main/java/com/kg/projects/homework/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.kg.projects.homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+// http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
+// Switch off the Spring Boot security configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {   	
+    	http
+		.authorizeRequests()
+			.antMatchers("/login.html").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.authorizeRequests()	
+			.antMatchers("/h2-console/**").permitAll()
+			.and().csrf().ignoringAntMatchers("/h2-console/**")
+			.and().headers().frameOptions().sameOrigin()
+			.and()
+		.formLogin()
+			.loginPage("/login.html")
+			.failureForwardUrl("/login-error.html")
+			.and()
+		.logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/login.html")
+			.and()
+		.httpBasic();   
+    }
+
+    @SuppressWarnings("deprecation")
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+        		.passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("DreadSith").password("dreadme").roles("USER");
+    }
+
+}
